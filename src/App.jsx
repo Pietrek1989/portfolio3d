c8792ae01d51d084cf5811070c06b6c59eb4a24d
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-05-26 19:16:40 +0200

    added social links

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
           <div className="relative z-0">
             <Contact />
             <StarsCanvas />
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
