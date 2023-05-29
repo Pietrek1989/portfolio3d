02aeafd70de380c318384b6782c67b172b18d829
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-05-29 12:28:44 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,16 +44,16 @@ const Contact = () => {
     }
     emailjs
       .send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Piotr Rodzen",
           from_email: form.email,
-          to_email: process.env.REACT_APP_EMAIL_ADDRESS,
+          to_email: import.meta.env.VITE_APP_EMAIL_ADDRESS,
           message: form.message,
         },
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
